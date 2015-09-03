496b132380619be10244401aaa9e8b358daee449
supersonic902 <dalton.harmon921@gaston.k12.nc.us>
2015-09-03 13:17:39 -0400

    Create Console.java
    creates the console

---
+++ Console.java
@@ -0,0 +1,16 @@
+Public_void_static [
+  print("hello") spawn (console) (var_1= "help", var_2 = "scan all", var_3 = "scan specific")
+    input console = press_button_1(var_1), press_button_2(var_2), press_button_3(var_3)
+    if input = var_1
+        then print (scan specific needs the file name you want checked, and scan all does not)
+    if input = var_2
+        then pull_files [database = CPU]
+            then scan [open(python part 1.python , set_database (CPU , all files) , run_program = true
+    if input = var_3
+        then spawn (console) [
+        print ("what is the file name and don't forget the . and file type")
+          input_console = spawn ( input_bar)
+        read (input)
+        pull_file = input_console
+                              ]
+                    ]
