2176e1e6cce1c76788d20120febfecf7342691a9
supersonic902 <dalton.harmon921@gaston.k12.nc.us>
2015-09-08 13:41:49 -0400

    Create signatures_lookfor.java

---
+++ signatures_lookfor.java
@@ -0,0 +1,8 @@
+Input = Console.java, set_this(Input) to "var1" 
+int get_file(var1)   //this file is version 1.0 build 1 of my own scan function in java
+int scan_this(var1)
+{if line (1241)"8a4600a200018b4601a30101b8cc4bcd" = true
+  then get_file(var1) set_this(file = var1) to "Virus"
+    then int del "Virus"  //This should delete the virus with that signature
+      then print."virus found and deleted"
+        then end program
