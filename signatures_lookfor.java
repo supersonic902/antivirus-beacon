28f95389a7a8c244209ac5b60f6c3d82dc8e91c3
supersonic902 <dalton.harmon921@gaston.k12.nc.us>
2015-09-10 11:19:48 -0400

    Update signatures_lookfor.java

--- signatures_lookfor.java
+++ signatures_lookfor.java
@@ -1,8 +1,21 @@
-Input = Console.java, set_this(Input) to "var1" 
+ready."input" = Console.java, set."Input" = "var1" 
 int get_file(var1)   //this file is version 1.0 build 1 of my own scan function in java
 int scan_this(var1)
-{if line (1241)"8a4600a200018b4601a30101b8cc4bcd" = true
-  then get_file(var1) set_this(file = var1) to "Virus"
-    then int del "Virus"  //This should delete the virus with that signature
-      then print."virus found and deleted"
-        then end program
+if line.(1241)"8a4600a200018b4601a30101b8cc4bcd" = true
+  get_file(var1) set.("file" = "var1") = "Virus"
+    int delete."Virus"  //This should delete the virus with that signature
+      print."virus found and deleted"
+        int finish
+  else int continue 
+if hash_value(#2) = 03b880022d0300a31304b106d3e08ec0a3a47cbe007ebf0000b9000cfcf3a4ff2ea27cb80000cd13
+  get_file(var1) set.("file" = "var1") = "Virus"
+    int delete."Virus"
+      print."virus found and deleted"
+        int finish       //file update now ver. 1.00.00.095 build 1
+  else int continue  
+        create.database {
+          bin.name("beacon antivirus database 1.0")
+            content{
+              
+            }
+        }
